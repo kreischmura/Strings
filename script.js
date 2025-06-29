35761e54ed235d427daf56a25749628c42009b01
kreischmura <karina.chmura@escola.pr.gov.br>
2025-06-29 18:54:10 -0300

    quarto commit

--- script.js
+++ script.js
@@ -12,26 +12,44 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
+
     const frequencias = contaFrequencias(palavras);
     let ordenadas = Object.keys(frequencias).sort(ordenador);
 
-function ordenador(p1, p2){
-    return frequencias[p2] - frequencias[p1];
-}
+    function ordenador(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
 
     console.log(ordenadas);
-    return ordenadas.slice(0,10);
+    return ordenadas.slice(0, 10);
 }
 
 function contaFrequencias(palavras) {
     let frequencias = {};
     for (let i of palavras) {
-       frequencias[i] = 0;
+        frequencias[i] = 0;
         for (let j of palavras) {
-           if (i == j) {
-               frequencias[i]++;
-           }
-       }
+            if (i == j) {
+                frequencias[i]++;
+            }
+        }
     }
-        return frequencias;
+    return frequencias;
+}
+
+function tiraPalavrasRuins(palavras) {
+    const PALAVRAS_RUINS = new Set(["para", "uma", "nós"]);
+    const palavrasBoas = [];
+    for (let palavra of palavras) {
+        if (!PALAVRAS_RUINS.has(palavra) && palavra.length > 2) {
+            palavrasBoas.push(palavra);
+        }
+    }
+    return palavrasBoas;
 }
