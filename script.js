080c35acbb28c691a38a58abb8c0ea4b652d5979
kreischmura <karina.chmura@escola.pr.gov.br>
2025-06-29 18:09:48 -0300

    terceiro commit

--- script.js
+++ script.js
@@ -12,16 +12,26 @@ function mostraPalavrasChave() {
 
 function processaTexto(texto) {
     let palavras = texto.split(/\P{L}+/u);
-     let frequencias = {};
-    for (let i of palavras) {
-        frequencias[i] = 0;
-         for (let j of palavras) {
-            if (i == j) {
-                frequencias[i]++;
-            }
-        }
-    }
-        console.log(frequencias);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenador);
 
-    return palavras;
+function ordenador(p1, p2){
+    return frequencias[p2] - frequencias[p1];
+}
+
+    console.log(ordenadas);
+    return ordenadas.slice(0,10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
+    for (let i of palavras) {
+       frequencias[i] = 0;
+        for (let j of palavras) {
+           if (i == j) {
+               frequencias[i]++;
+           }
+       }
+    }
+        return frequencias;
 }
